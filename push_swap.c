f1b42725f40ff759032ad8d14e6cb7cb85c677a3
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-04-24 18:19:23 +0100

    first compiling version

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 19:49:48 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/22 21:49:30 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:50:49 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,70 +15,71 @@
 
 int main(int argc, char **argv)
 {
-    t_stack *head_a;
-    t_stack *head_b;
+	t_stack *head_a;
+	//t_stack *head_b;
 
-    if (argc == 1)
-        return 0;
-    head_a = 0;
-    head_b = 0;
-    /* Read input */
-    ft_listinput(argv, argc, &head_a);
-    ft_printlist(*head_a);
-    /* Check for dups, max min int */
+	if (argc == 1)
+		return 0;
+	head_a = 0;
+	//head_b = 0;
+	/* Read input */
+	ft_listinput(argv, argc, &head_a);
+	ft_printlist(&head_a);
+	/* Check for dups, max min int */
 }
 
 void    ft_listinput(char **argv, int argc, t_stack **head_a)
 {
-    int    i;
+	int	i;
 
-    if (argc == 1)
-        ft_listinput(ft_split(argv[1], ' '), ft_charsize(ft_split(argv[1], ' ')), head_a);
-    else {
-		i = 0;
-		while (i < argc)
-		{
-			ft_lstnewd(head_a, ft_atol(argv[i++]));
-
-		}
+	if (argc == 2)
+		ft_listinput(ft_split(argv[1], ' '), ft_charsize(ft_split(argv[1], ' ')), head_a);
+	else {
+		i = 0;//Fix indexation for argv and ft_split (start with i = 1 or i = 0)
+		while (i < 4)
+			ft_addtail(head_a, ft_atol(argv[i++]));
 	}
 }
 
-int     ft_charsize(char **str)
+int	ft_charsize(char **str)
 {
-    int     size;
+	int	size;
 
-    size = 0;
-    while(str[size])
-        size++;
-    return (++size);
+	size = 0;
+	while(str[size])
+		size++;
+	return (++size);
 }
 
-void    ft_printlist(t_stack head_a)
+void    ft_printlist(t_stack **head_a)
 {
-    ft_printf("%i", head_a.content);
-    if(head_a.next)
-        head_a = *head_a.next;
-	else
+	ft_printf("%i\n", (*head_a)->content);
+	if((*head_a)->next)
 	{
+		(*head_a) = (*head_a)->next;
 		ft_printlist(head_a);
-		return ;
 	}
-    ft_printlist(head_a);
+	return ;
 }
 
-void	ft_lstnewd(t_stack **head, long content)
+void	ft_addtail(t_stack **head, long content)
 {
 	t_stack	*lst;
 	t_stack *current = *head; // save pointer
 	lst = malloc(sizeof(t_stack));
 	if (!lst)
-		return (NULL);
+		return ;// exit
 	lst->content = content;
 	lst->next = NULL;
 	// lista vazia
-	(*head)->next = lst;
+	if(!(*head))
+	{
+		free(*head);
+		*head = lst;
+		return ;
+	}
 	// list nao vazia percorrer ate ao proximo ser nullo Head A B |C|-> D
-	while()
-
+	while(current->next)
+		current = current->next;
+	current->next = lst;
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 20:04:26 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/22 20:05:23 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:44:30 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ typedef struct s_stack
 {
     long     content;
     struct s_stack	*next;
-}				t_stack;
+}	t_stack;
 
-void    ft_listinput(char **argv, int argc, t_stack head_a);
-int     ft_charsize(char **str);
-void    ft_printlist(t_stack head_a);
-void	ft_lstnewd(t_stack **head, long content);
+int	ft_charsize(char **str);
+void	ft_listinput(char **argv, int argc, t_stack **head_a);
+void	ft_printlist(t_stack **head_a);
+void	ft_addtail(t_stack **head, long content);
 
 #endif

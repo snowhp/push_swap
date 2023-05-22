b3568d31e8eeb6b5e7401c107d01019ec9d96c38
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-05-22 17:44:26 +0100

    Segfault fix

--- error/error.c
+++ error/error.c
@@ -6,29 +6,35 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:31:49 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/22 17:13:14 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/22 17:43:34 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	ft_exit(char *erromsg, int exitcode, t_stack **head_a, t_stack **head_b)
+void	ft_exit(char *erromsg, int c, t_stack **head_a, t_stack **head_b)
 {
 	t_stack	*next;
 
-	if (exitcode != 0)
+	if (c != 0)
 		ft_printf("Error\n%s", erromsg);
-	while (*head_a != NULL)
+	if (head_a)
 	{
-		next = (*head_a)->next;
-		free(*head_a);
-		*head_a = next;
+		while (*head_a != NULL)
+		{
+			next = (*head_a)->next;
+			free(*head_a);
+			*head_a = next;
+		}
 	}
-	while (*head_b != NULL)
+	if (head_b)
 	{
-		next = (*head_b)->next;
-		free(*head_b);
-		*head_b = next;
+		while (*head_b != NULL)
+		{
+			next = (*head_b)->next;
+			free(*head_b);
+			*head_b = next;
+		}
 	}
-	exit (exitcode);
+	exit (c);
 }

8a672b594a8bdcbe7b63888a41a9a28800ce4275
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-05-22 14:22:31 +0100

    Sort 4 and 5

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 20:04:26 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/18 18:23:12 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/22 14:09:46 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,26 @@ typedef struct s_stack
 }	t_stack;
 
 int	ft_charsize(char **str);
-void    ft_listinput(char **argv, int argc, t_stack **head_a, int i);
+void	ft_listinput(char **argv, int argc, t_stack **head_a, int i);
 void	printlist(t_stack **head_a, t_stack **head_b);
 void	ft_addtail(t_stack **head, long content);
-void	ft_exit(t_stack **head_a, t_stack **head_b, char **error);
-void check_arg(char **argv);
-void ft_checkdigit(char **argv);
-void ft_checkmax(char **argv);
-void ft_insertlist(t_stack **head_a, char **argv);
+void	check_arg(char **argv);
+void	ft_checkdigit(char **argv);
+void	ft_checkmax(char **argv);
+void	ft_insertlist(t_stack **head_a, char **argv);
 void	ft_push(t_stack **head_o, t_stack **head_d, char c);
 void	ft_swap(t_stack **head_1, char c);
 void	ft_rotateup(t_stack **head_1, char c);
 void	ft_rotatedown(t_stack **head_1, char c);
-void ft_sortlist(t_stack **head_a,t_stack **head_b);
-int     ft_listsize(t_stack **head_a);
-void    ft_sort2(t_stack **head_a);
+void	ft_sortlist(t_stack **head_a,t_stack **head_b);
+int	ft_listsize(t_stack **head_a);
+void	ft_sort2(t_stack **head_a);
 void	ft_checkdups(t_stack **head_a);
-void    ft_sort3(t_stack **head);
-int		ft_listsorted(t_stack **head);
+void	ft_sort3(t_stack **head);
+int	ft_listsorted(t_stack **head);
 void	ft_sort5(t_stack **head_a, t_stack **head_b, int size);
+void	ft_insertinsortlist(t_stack **head_a, t_stack **head_b);
+int	ft_findmax(t_stack **head);
+void	ft_exit(char *erromsg, int exitcode);
 
 #endif
--- sort/sort.c
+++ sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 12:56:03 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/18 21:40:10 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/22 14:20:55 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,36 +33,72 @@ void	ft_sort5(t_stack **head_a, t_stack **head_b, int size)
 	if (size == 5)
 		ft_push(head_a, head_b, 'b');
 	ft_sort3(head_a);
-	ft_rotateandpush(head_a, head_b);
+	ft_insertinsortlist(head_a, head_b);
+	if (size == 5)
+		ft_insertinsortlist(head_a, head_b);
 }
 
-void	ft_rotateandpush(t_stack **head_a, t_stack **head_b)
+void	ft_insertinsortlist(t_stack **head_a, t_stack **head_b)
 {
-	/* TODO */
+	int	max;
+	unsigned int	rotates;
+
+	max = ft_findmax(head_a);
+	if ((*head_b)->content > max)
+	{
+		ft_push(head_b, head_a, 'a');
+		ft_rotateup(head_a, 'a');
+		return ;
+	}
+	rotates = 0;
+	while((*head_b)->content > (*head_a)->content)
+	{
+		ft_rotateup(head_a, 'a');
+		rotates++;
+	}
+	ft_push(head_b, head_a, 'a');
+	while(rotates--)
+		ft_rotatedown(head_a, 'a');
 }
 
-int     ft_listsize(t_stack **head_a)
+int	ft_findmax(t_stack **head)
 {
-    t_stack *current;
-    int     size;
+	t_stack *current;
+	int		max;
 
-    size = 0;
-    current = *head_a;
-    while (current)
-    {
-        size++;
-        current = current->next;
-    }
-    return (size);
+	current = *head;
+	max = current->content;
+	while(current)
+	{
+		if (max < current->content)
+			max = current->content;
+		current = current->next;
+	}
+	return (max);
 }
 
-void    ft_sort2(t_stack **head_a)
+int	ft_listsize(t_stack **head_a)
 {
-    if((*head_a)->next->content < (*head_a)->content)
-        ft_rotatedown(head_a, 'a');
+	t_stack *current;
+	int	size;
+
+	size = 0;
+	current = *head_a;
+	while (current)
+	{
+		size++;
+		current = current->next;
+	}
+	return (size);
 }
 
-void    ft_sort3(t_stack **head)
+void	ft_sort2(t_stack **head_a)
+{
+	if((*head_a)->next->content < (*head_a)->content)
+		ft_rotatedown(head_a, 'a');
+}
+
+void	ft_sort3(t_stack **head)
 {
 	long f;
 	long s;
@@ -89,7 +125,7 @@ void    ft_sort3(t_stack **head)
 	}
 }
 
-int		ft_listsorted(t_stack **head)
+int	ft_listsorted(t_stack **head)
 {
 	t_stack *current;
 

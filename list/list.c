68f684c5eede5deac4c28edeb4f5f81df2a4b894
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-05-26 11:38:12 +0100

    file struct changed

---
+++ list/list.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 10:44:08 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/05/26 10:44:19 by tde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	ft_insertlist(t_stack **head_a, char **argv, int x)
+{
+	int	nb;
+
+	ft_checkdigit(argv, x);
+	ft_checkmax(argv, x);
+	while (argv[x])
+	{
+		nb = ft_atoi(argv[x++]);
+		ft_addtail(head_a, nb);
+	}
+	if (ft_listsize(head_a) == 0)
+		ft_exit(1, head_a, NULL);
+	if (ft_listsize(head_a) == 1)
+		ft_exit(0, head_a, NULL);
+	ft_checkdups(head_a);
+}
+
+void	printlist(t_stack **head_a, t_stack **head_b)
+{
+	t_stack	*current;
+
+	current = *head_a;
+	while (current != NULL)
+	{
+		ft_printf("%d ", current->content);
+		current = current->next;
+	}
+	ft_printf("A");
+	ft_printf("\n");
+	current = *head_b;
+	while (current != NULL)
+	{
+		ft_printf("%d ", current->content);
+		current = current->next;
+	}
+	ft_printf("B");
+	ft_printf("\n");
+}
+
+void	ft_addtail(t_stack **head, long content)
+{
+	t_stack	*lst;
+	t_stack	*current;
+
+	current = *head;
+	lst = malloc(sizeof(t_stack));
+	if (!lst)
+		return ;
+	lst->content = content;
+	lst->next = NULL;
+	lst->index = 0;
+	if (!(*head))
+	{
+		free(*head);
+		*head = lst;
+		return ;
+	}
+	while (current->next)
+		current = current->next;
+	current->next = lst;
+}
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 19:49:48 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/26 09:54:15 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/26 10:56:33 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	head_a = 0;
 	head_b = 0;
 	if (argv[1][0] == 0)
-		return (0);
+		ft_exit(1, &head_a, &head_b);
 	if (argc == 2)
 	{
 		argv2 = ft_split(argv[1], ' ');
@@ -39,67 +39,4 @@ int	main(int argc, char **argv)
 		ft_insertlist(&head_a, argv, 1);
 	ft_sortlist(&head_a, &head_b);
 	ft_exit(0, &head_a, &head_b);
-}
-
-void	ft_insertlist(t_stack **head_a, char **argv, int x)
-{
-	int	nb;
-
-	ft_checkdigit(argv, x);
-	ft_checkmax(argv, x);
-	while (argv[x])
-	{
-		nb = ft_atoi(argv[x++]);
-		ft_addtail(head_a, nb);
-	}
-	if (ft_listsize(head_a) == 0)
-		ft_exit(1, head_a, NULL);
-	if (ft_listsize(head_a) == 1)
-		ft_exit(0, head_a, NULL);
-	ft_checkdups(head_a);
-}
-
-void	printlist(t_stack **head_a, t_stack **head_b)
-{
-	t_stack	*current;
-
-	current = *head_a;
-	while (current != NULL)
-	{
-		ft_printf("%d ", current->content);
-		current = current->next;
-	}
-	ft_printf("A");
-	ft_printf("\n");
-	current = *head_b;
-	while (current != NULL)
-	{
-		ft_printf("%d ", current->content);
-		current = current->next;
-	}
-	ft_printf("B");
-	ft_printf("\n");
-}
-
-void	ft_addtail(t_stack **head, long content)
-{
-	t_stack	*lst;
-	t_stack	*current;
-
-	current = *head;
-	lst = malloc(sizeof(t_stack));
-	if (!lst)
-		return ;
-	lst->content = content;
-	lst->next = NULL;
-	lst->index = 0;
-	if (!(*head))
-	{
-		free(*head);
-		*head = lst;
-		return ;
-	}
-	while (current->next)
-		current = current->next;
-	current->next = lst;
 }

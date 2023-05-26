777f33d766dbf3e7caace374f91ee988369ac10f
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-05-26 11:38:01 +0100

    ss movement added

--- moves/moves1.c
+++ moves/moves1.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:03:58 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/25 17:04:33 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/26 11:16:21 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ void	ft_doublerevrotate(t_stack **head_a, t_stack **head_b)
 	ft_rotatedown(head_a, 0);
 	ft_rotatedown(head_b, 0);
 	ft_printf("rrr\n");
+}
+
+void	ft_doubleswap(t_stack **head_a, t_stack **head_b)
+{
+	ft_swap(head_a, 0);
+	ft_swap(head_b, 0);
+	ft_printf("ss\n");
 }
